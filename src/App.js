4c0fa1a11f5b8f736da2d8eab84055d35d69ba9f
kejikodo <kawaikeiji.22@gmail.com>
2023-12-07 17:12:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import ImageGrallery from './ImageGrallery';
+import { useRef, useState } from 'react';
+
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    //APIURL
+    const endpointURL =
+    `https://pixabay.com/api/?key=41119626-55ca3ff5c5f5e058ecc9c9ad6&q=${ref.current.value}&image_type=photo`;
+
+    //APIを叩く
+    fetch(endpointURL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.hits)
+      setFetchData(data.hits);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>画像検索アプリ</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='画像を探す' ref={ref} />
+      </form>
+      <ImageGrallery fetchData={fetchData} />
     </div>
   );
 }
---
+++ src/ImageGrallery.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ImageGrallery.css';
+
+const Gallery = ({ fetchData }) => {
+  return (
+    <div>
+      <div className='images-wrapper'>
+        {fetchData.map((data) => (
+          <div className='image' key={data.id}>
+            <a href={data.pageURL} target='_blank' rel='noreferrer'>
+              <img src={data.largeImageURL} alt='' />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Gallery;
